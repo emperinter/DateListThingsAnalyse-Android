6359c8d9e6f44720d86de2837ca6b9386e07749c
emperinter <hehiuzhang@gmail.com>
2022-02-12 17:38:06 +0800

    Finished Login Authing Function

--- app/src/main/java/info/emperinter/DateListThingsAnalyseAndroid/ApiActivity.java
+++ app/src/main/java/info/emperinter/DateListThingsAnalyseAndroid/ApiActivity.java
@@ -2,6 +2,8 @@ package info.emperinter.DateListThingsAnalyseAndroid;
 
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.util.Log;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -14,31 +16,29 @@ import java.nio.charset.StandardCharsets;
 public class ApiActivity {
     private String get_data = "";
 
+    //    HTTP响应流转换成JSON
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private JSONObject streamToJson(InputStream inputStream) throws Exception {
+    private JSONArray streamToJson(InputStream inputStream) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
         String temp;
         StringBuilder stringBuilder = new StringBuilder();
         while ((temp = bufferedReader.readLine()) != null) {
             stringBuilder.append(temp);
         }
-        return new JSONObject(stringBuilder.toString().trim());
+        return new JSONArray(stringBuilder.toString().trim());
     }
-
+    
     //从服务器获取数据（json）
-    public String request(final String host,final int m, final String get) { //m = 0 时为默认的更新，m = 1时调用的为查询语句
+    public String request(final String strUrl) {
         //新的线程
         new Thread(new Runnable() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void run() {
-                String strUrl = "";
                 try {
-                    if (m == 1) {
-                        strUrl = "https://" + host + get ;
-                    } else if (m == 2) {
-                        strUrl = "https://" + host + get ;
-                    }
+//                    strUrl = "https://plan.emperinter.ga/api/user/query/?format=json&user_name=emperinter&user_passwd=emperinter";
+//                    strUrl = "https://plan.emperinter.ga/api/thing/query/?format=json&userid=10";
+                    Log.v("HTTP-getStream",strUrl);
                     URL url = new URL(strUrl);
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET"); // 设置请求方式为 GET
@@ -46,17 +46,21 @@ public class ApiActivity {
                     int responseCode = connection.getResponseCode();
                     if (responseCode == HttpURLConnection.HTTP_OK) { // 请求成功
                         InputStream inputStream = connection.getInputStream(); // 得到响应流
-                        JSONObject json = null; // 从响应流中提取 JSON
-                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-                            json = streamToJson(inputStream);
-                        }
-                        get_data = json.getString("things");
+                        Log.v("inputStream-getStream:" , inputStream.toString());
+                        JSONArray json = null;
+                        json = streamToJson(inputStream);
+
+//                        System.out.printf("\n####################################################");
+//                        System.out.printf("getStream-things_id:\t"+json.getJSONObject(0).get("things_id") + "\t" + String.valueOf(json.length()) );
+//                        System.out.printf("\n####################################################");
+                        get_data = json.toString();
                     } else {
                         get_data = "NotFound!";
                     }
                     connection.disconnect();
                 } catch (Exception e) {
-                    get_data = "<div align ='center'>Connect Error!</div>";
+                    Log.v("Exception-getStream",e.toString());
+                    get_data = e.toString();
                 }
             }
         }).start();
--- app/src/main/java/info/emperinter/DateListThingsAnalyseAndroid/LoginActivity.java
+++ app/src/main/java/info/emperinter/DateListThingsAnalyseAndroid/LoginActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     private Button LoginBtn = null;
     private TextView user,passwd,host;
-    private String get_data = "";
+    private String reqGet = "";
+    private int userid;
     public ApiActivity api = new ApiActivity();
 
     @Override
@@ -29,14 +34,30 @@ public class LoginActivity extends AppCompatActivity {
         host = findViewById(R.id.host);
 
         LoginBtn.setOnClickListener(view -> {
-            Toast.makeText(this, user.getText()+"\n"+passwd.getText()+"\n"+host.getText(), Toast.LENGTH_SHORT).show();
-            if(api.request(host.toString(),1,"2") != ""){
-                Intent changeToMain = new Intent(this, ContainerActivity.class);
-                startActivity(changeToMain);
+//          strUrl = "https://plan.emperinter.ga/api/user/query/?format=json&user_name=emperinter&user_passwd=emperinter";
+            if (host.getText() != "" && user.getText() != "" && passwd.getText() != ""){
+                reqGet = api.request(host.getText()+"/api/user/query/?format=json&user_name="+user.getText()+"&user_passwd="+passwd.getText());
+                Log.v("reqGet-getStream",reqGet);
+                if(reqGet.contains("user_id")){
+                    try {
+                        JSONArray user = new JSONArray(reqGet);
+                        userid = (int) user.getJSONObject(0).get("user_id");
+                        Log.v("reqGet-getStream-userid",String.valueOf(userid));
+                        Intent changeToMain = new Intent(this, ContainerActivity.class);
+                        startActivity(changeToMain);
+                    } catch (JSONException e) {
+//                        e.printStackTrace();
+                        Toast.makeText(this,e.toString(),Toast.LENGTH_SHORT).show();
+                    }
+                }else if(reqGet.contains("[]")){
+                    Toast.makeText(this,"username or password is wrong !",Toast.LENGTH_SHORT).show();
+                }else if(reqGet.contains("HTTP")){
+                    Toast.makeText(this," Cleartext HTTP traffic to "+host.getText()+" not permitted",Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(this,"Please Input Your Information !",Toast.LENGTH_SHORT).show();
             }
         });
     }
-
-
 }
 
